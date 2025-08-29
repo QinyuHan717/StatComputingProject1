53520511644f37e76013d88eac9f05b1d3805215
Rita Han <ritahanbj@gmail.com>
2025-08-29 15:59:47 -0400

    Added steps for configuring our git repo with GitHub

--- setup_git.R
+++ setup_git.R
@@ -1,2 +1,10 @@
 ## Start to use version control
 usethis::use_git()
+
+## After the restart, we next need to change the default Git branch to "gh-pages".
+## This will make our lives a bit easier later on, as this specific name is used by
+## GitHub pages https://pages.github.com/ for websites.
+usethis::git_default_branch_rename(to = "gh-pages")
+
+## Upload to GitHub
+usethis::use_github()
